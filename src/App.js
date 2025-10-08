c671ca273b494ba3676228dc3845cb66bf347f09
huelgasjames <huelgasjames46@gmail.com>
2025-10-08 16:51:17 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Courses from "./pages/Courses";
-import Admission from "./pages/Admission";
-
 
 function App() {
   return (
@@ -14,9 +10,6 @@ function App() {
       <div className="container mt-4">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/admission" element={<Admission />} />
         </Routes>
       </div>
       <Footer />
